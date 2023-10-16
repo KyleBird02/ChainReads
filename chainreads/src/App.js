671fd1ecc3d203eb8797041e1eb55b30b54698c3
Kyle <81643823+KyleBird02@users.noreply.github.com>
2023-10-16 23:15:11 +0530

    Minor UI Push

--- chainreads/src/App.js
+++ chainreads/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import BookList from "./components/BookList";
 import AddBookForm from "./components/AddBookForm";
+import contract from "./contract";
 
 function App() {
     return (
